92f1aa92edc1d2245661861df7ce56ace5c8e70a
DANIELS-PC\djalt <dj.altamirano2013@gmail.com>
2026-04-28 11:28:12 -0600

    Test 2

--- api.test.js
+++ api.test.js
@@ -5,3 +5,8 @@ test('GET / Devuelve este mensaje!', async () => {
     const res = await request(app).get('/');
     expect(res.text).toBe('Hello World!');
 });
+
+test('GET /suma Devuelve la suma de dos números', async () => {
+    const res = await request(app).get('/suma').query({ a: 5, b: 3 });
+    expect(res.text).toBe('La suma de 5 y 3 es 8');
+});
